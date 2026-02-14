d89224dd1b990f2fde68dd5767819930f12d17f7
Vinh Nguyen <vinh.ntuan@gmail.com>
2026-02-14 23:05:22 +0100

    [Fix] Add network security group Allow SSH

--- main.tf
+++ main.tf
@@ -17,6 +17,56 @@ resource "azurerm_subnet" "this" {
   address_prefixes     = ["10.0.1.0/24"]
 }
 
+resource "azurerm_network_security_group" "this" {
+  name                = "assessment-subnet-nsg"
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+
+  # Rule for HTTP
+  security_rule {
+    name                       = "AllowHTTPInbound"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  # Rule for HTTPS
+  security_rule {
+    name                       = "AllowHTTPSInbound"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  # SSH Rule, dev only
+  security_rule {
+    name                       = "AllowSSHInbound"
+    priority                   = 120
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "this" {
+  subnet_id                 = azurerm_subnet.this.id
+  network_security_group_id = azurerm_network_security_group.this.id
+}
+
 resource "azurerm_public_ip" "this" {
   count               = var.vm_count
   name                = "assessment-vm.pip-${count.index}"
